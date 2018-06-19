e95af83036d4a7a19f13b61e478c5d80fe678e6c
Nathen Harvey <nharvey@chef.io>
2018-06-18 23:55:55 -0400

    Flesh out the set-up a bit more
    Signed-off-by: Nathen Harvey <nharvey@chef.io>

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,8 @@
+# set a minimum requirement for the version of terraform
+terraform {
+  required_version = "~> 0.11.0"
+}
+
 # Specify the provider and access details
 provider "aws" {
   region = "${var.aws_region}"
@@ -5,7 +10,7 @@ provider "aws" {
 
 # Create a VPC to launch our instances into
 resource "aws_vpc" "testing_terraform" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "${var.vpc_cidr}"
 }
 
 # Create an internet gateway to give our subnet access to the outside world
@@ -28,9 +33,9 @@ resource "aws_subnet" "default" {
 }
 
 # A security group for the ELB so it is accessible via the web
-resource "aws_security_group" "elb" {
+resource "aws_security_group" "world_to_elb" {
   name        = "testing_terraform_elb"
-  description = "Used in the terraform"
+  description = "World-facing ELB SG for Testing Terraform Presentation.  nharvey contact"
   vpc_id      = "${aws_vpc.testing_terraform.id}"
 
   # HTTP access from anywhere
@@ -52,25 +57,38 @@ resource "aws_security_group" "elb" {
 
 # Our default security group to access
 # the instances over SSH and HTTP
-resource "aws_security_group" "default" {
+resource "aws_security_group" "lb_to_webservers" {
   name        = "testing_terraform"
-  description = "Used in the terraform"
+  description = "Used in testing terraform"
   vpc_id      = "${aws_vpc.testing_terraform.id}"
-
-  # SSH access from anywhere
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   # HTTP access from the VPC
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["${var.vpc_cidr}"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ssh_from_office" {
+  name   = "testing-terraform-ssh"
+  vpc_id = "${aws_vpc.testing_terraform.id}"
+
+  # SSH access from special office addresses
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = "${var.ssh_cidrs}"
   }
 
   # outbound internet access
@@ -86,7 +104,10 @@ resource "aws_elb" "web" {
   name = "testing-terraform-elb"
 
   subnets         = ["${aws_subnet.default.id}"]
-  security_groups = ["${aws_security_group.elb.id}"]
+  security_groups = [
+    "${aws_security_group.lb_to_webservers.id}",
+    "${aws_security_group.world_to_elb.id}"
+  ]
   instances       = ["${aws_instance.web.id}"]
 
   listener {
@@ -98,15 +119,6 @@ resource "aws_elb" "web" {
 }
 
 resource "aws_instance" "web" {
-  # The connection block tells our provisioner how to
-  # communicate with the resource (instance)
-  connection {
-    # The default username for our AMI
-    user = "ubuntu"
-
-    # The connection will use the local SSH agent for authentication.
-  }
-
   instance_type = "t2.micro"
 
   # Lookup the correct AMI based on the region
@@ -116,13 +128,32 @@ resource "aws_instance" "web" {
   # The name of our SSH keypair.
   key_name = "${var.key_name}"
 
-  # Our Security group to allow HTTP and SSH access
-  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  # Our Security groups to allow HTTP and SSH access
+  vpc_security_group_ids = [
+    "${aws_security_group.lb_to_webservers.id}",
+    "${aws_security_group.ssh_from_office.id}",
+  ]
 
   # We're going to launch into the same subnet as our ELB. In a production
   # environment it's more common to have a separate private subnet for
   # backend instances.
   subnet_id = "${aws_subnet.default.id}"
+
+  tags {
+    X-TTL         = "2018-06-19"
+    X-Dept        = "Community Engineering"
+    X-Application = "Testing Terraform"
+    X-Contact     = "nharvey"
+  }
+
+  # The connection block tells our provisioner how to
+  # communicate with the resource (instance)
+  connection {
+    # The default username for our AMI
+    user = "ubuntu"
+
+    # The connection will use the local SSH agent for authentication.
+  }
 
   # We run a remote provisioner on the instance after creating it.
   # In this case, we just install nginx and start it. By default,
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,6 @@
 variable "public_key_path" {
+  default = "/Users/nathenharvey/.aws/nathenharvey-community.pem"
+
   description = <<DESCRIPTION
 Path to the SSH public key to be used for authentication.
 Ensure this keypair is added to your local SSH agent so provisioners can
@@ -9,12 +11,21 @@ DESCRIPTION
 }
 
 variable "key_name" {
+  default     = "nathenharvey-community.pem"
   description = "Desired name of AWS key pair"
 }
 
 variable "aws_region" {
   description = "AWS region to launch servers."
   default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "ssh_cidrs" {
+  default = ["173.239.212.22/32"]
 }
 
 # Ubuntu Xenial 16.04 LTS (x64)
